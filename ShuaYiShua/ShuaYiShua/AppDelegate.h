64a3822f297d57430e1cde570e237b8619610ea6
HaixiaLei <heguiting@126.com>
2015-08-25 13:09:18 +0800

    新建ShuaYiShua

---
+++ ShuaYiShua/ShuaYiShua/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShuaYiShua
+//
+//  Created by Sywine on 8/25/15.
+//  Copyright (c) 2015 Sywine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShuaYiShua/ShuaYiShua/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShuaYiShua
+//
+//  Created by Sywine on 8/25/15.
+//  Copyright (c) 2015 Sywine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
