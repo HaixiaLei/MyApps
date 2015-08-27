26276986010147addcea313e53bc9d7d959504f2
HaixiaLei <heguiting@126.com>
2015-08-27 16:27:41 +0800

    架构3

---
+++ ShuaYiShua/ShuaYiShua/LoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.h
+//  ShuaYiShua
+//
+//  Created by Sywine on 8/27/15.
+//  Copyright (c) 2015 Sywine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+- (IBAction)login:(UIButton *)sender;
+@end
---
+++ ShuaYiShua/ShuaYiShua/LoginViewController.m
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.m
+//  ShuaYiShua
+//
+//  Created by Sywine on 8/27/15.
+//  Copyright (c) 2015 Sywine. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import "MacroDefinitions.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)login:(UIButton *)sender {
+    LOG(@"%@触发方法：%@",NSStringFromClass([self class]),NSStringFromSelector(_cmd));
+    [self.navigationController dismissViewControllerAnimated:YES completion:nil];
+}
+@end
